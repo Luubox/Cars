779ac10c9642a2c6b1beaf3b0987f304957c45a1
J Nielsen <buhros@hotmail.com>
2018-09-21 10:42:01 +0200

    Car class specified

--- Cars/Car.cs
+++ Cars/Car.cs
@@ -8,5 +8,48 @@ namespace Cars
 {
     class Car
     {
+        private string _make;
+        private string _model;
+        private int _year;
+        private bool _fuelType;
+        private string _colour;
+        private string _licensePlate;
+
+        public string Make
+        {
+            get { return _make; } //behøver ikke set, skal ikke kunne ændres efterfølgende
+        }
+        public string Model
+        {
+            get { return _model; }
+        }
+        public int Year
+        {
+            get { return _year; }
+        }
+        public bool FuelType
+        {
+            get { return _fuelType; }
+        }
+        public string Colour
+        {
+            get { return _colour; }
+            set { _colour = value; }
+        }
+        public string LicensePlate
+        {
+            get { return _licensePlate; }
+            set { _licensePlate = value; }
+        }
+
+        public double Drive()
+        {
+            return 0.0;
+        }
+
+        public void Gear()
+        {
+            
+        }
     }
 }
